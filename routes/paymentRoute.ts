27287f7d0d2f1359699745fb0b658417e9cc2e7d
munene-m <munenenjue18@gmail.com>
2023-11-22 16:40:55 +0300

    chore: get callbackresponse

--- routes/paymentRoute.ts
+++ routes/paymentRoute.ts
@@ -1,10 +1,15 @@
 import express from "express";
 const router = express.Router();
 // const { protect, mpesaProtect } = require('../middleware/authMiddleware')
-import { makePayment, handleCallback } from "../controllers/paymentController";
+import {
+  makePayment,
+  handleCallback,
+  getCallbackResponse,
+} from "../controllers/paymentController";
 import generateToken from "../middleware/paymentMiddleware";
 
 router.route("/stk").post(generateToken, makePayment);
 router.route("/callback").post(handleCallback);
+router.route("/getPayments").get(getCallbackResponse);
 
 export default router;
