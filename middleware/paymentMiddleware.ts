51d49fb3882ae4c07337d59ec5e321e1cf02f94a
munene-m <munenenjue18@gmail.com>
2023-11-22 16:40:35 +0300

    chore: put async code in try catch

--- middleware/paymentMiddleware.ts
+++ middleware/paymentMiddleware.ts
@@ -7,26 +7,25 @@ const generateToken = async (
   res: Response,
   next: NextFunction
 ) => {
-  const key = process.env.CONSUMER_KEY;
-  const secret = process.env.CONSUMER_SECRET;
-  const auth = Buffer.from(`${key}:${secret}`).toString("base64");
-  await axios
-    .get(
+  try {
+    const key = process.env.CONSUMER_KEY;
+    const secret = process.env.CONSUMER_SECRET;
+    const auth = Buffer.from(`${key}:${secret}`).toString("base64");
+    const headers = {
+      Authorization: "Basic" + " " + auth,
+      "Content-Type": "application/json",
+    };
+    const response = await axios.get(
       "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
       {
-        headers: {
-          Authorization: `Basic ${auth}`,
-        },
+        headers,
       }
-    )
-    .then((response) => {
-      req.token = response.data.access_token;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-      // return
-      // res.status(400).json(err.message)
-    });
+    );
+    console.log(response.data);
+    req.token = response.data.access_token;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
 };
 export default generateToken;
