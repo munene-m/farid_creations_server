eb1f4e9f11afe8ef40b789ef354a66d9f6fc3f05
munene-m <munenenjue18@gmail.com>
2023-11-27 09:05:45 +0300

    chore: return error messages in json

--- utils/validation.ts
+++ utils/validation.ts
@@ -4,27 +4,27 @@ export function validateUserFields(email: string, password: string) {
   if (!email || !password) {
     switch (true) {
       case !email:
-        return "Email is required";
+        return { error: "Email is required" };
       case !password:
-        return "Password is required";
+        return { error: "Password is required" };
       default:
-        return "Please enter all required fields";
+        return { error: "Please enter all required fields" };
     }
   }
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   if (!email.match(emailRegex)) {
-    return "Invalid email address";
+    return { error: "Invalid email address" };
   }
   if (password.length < 8) {
-    return "Password must be at least 8 characters long";
+    return { error: "Password must be at least 8 characters long" };
   }
 
   if (!/[A-Z]/.test(password)) {
-    return "Password must contain at least one uppercase letter";
+    return { error: "Password must contain at least one uppercase letter" };
   }
 
   if (!/[!@#$%^&*]/.test(password)) {
-    return "Password must contain at least one special character";
+    return { error: "Password must contain at least one special character" };
   }
 
   return null; // No validation error
@@ -34,11 +34,11 @@ export function validateLoginFields(email: string, password: string) {
   if (!email || !password) {
     switch (true) {
       case !email:
-        return "Email is required";
+        return { error: "Email is required" };
       case !password:
-        return "Password is required";
+        return { error: "Password is required" };
       default:
-        return "Please enter all required fields";
+        return { error: "Please enter all required fields" };
     }
   }
   return null;
@@ -52,35 +52,35 @@ export function validateAdminRegistration(
   if (!phoneNumber || !email || !password) {
     switch (true) {
       case !phoneNumber:
-        return "Phone number is required";
+        return { error: "Phone number is required" };
       case !email:
-        return "Email is required";
+        return { error: "Email is required" };
       case !password:
-        return "Password is required";
+        return { error: "Password is required" };
       default:
-        return "Please enter all required fields";
+        return { error: "Please enter all required fields" };
     }
   }
 
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   if (!email.match(emailRegex)) {
-    return "Invalid email address";
+    return { error: "Invalid email address" };
   }
   const phoneRegex = /^(?:\+254|0)[17]\d{8}$/; // Matches +254 or 0, followed by 1 or 7, and then 8 digits
 
   if (!phoneNumber.match(phoneRegex)) {
-    return "Invalid phone number format";
+    return { error: "Invalid phone number format" };
   }
   if (password.length < 8) {
-    return "Password must be at least 8 characters long";
+    return { error: "Password must be at least 8 characters long" };
   }
 
   if (!/[A-Z]/.test(password)) {
-    return "Password must contain at least one uppercase letter";
+    return { error: "Password must contain at least one uppercase letter" };
   }
 
   if (!/[!@#$%^&*]/.test(password)) {
-    return "Password must contain at least one special character";
+    return { error: "Password must contain at least one special character" };
   }
 
   return null; // No validation error
@@ -90,19 +90,22 @@ export function validateProductFields(
   name: string,
   description: string,
   price: number,
+  type: string,
   image: Express.Multer.File | undefined
 ) {
-  if (!name || !price || !description || !image) {
+  if (!name || !price || !description || !image || !type) {
     if (!name) {
-      return "Item name is required";
+      return { error: "Item name is required" };
     } else if (!price) {
-      return "Price is required";
+      return { error: "Price is required" };
     } else if (!description) {
-      return "Item description is required";
+      return { error: "Item description is required" };
     } else if (!image) {
-      return "Image is required";
+      return { error: "Image is required" };
+    } else if (!type) {
+      return { error: "Product type is required" };
     } else {
-      return "Please enter all required fields";
+      return { error: "Please enter all required fields" };
     }
   }
   return null;
@@ -115,13 +118,13 @@ export function validateWorkFields(
 ) {
   if (!title || !description || !image) {
     if (!title) {
-      return "Item name is required";
+      return { error: "Item name is required" };
     } else if (!image) {
-      return "Image is required";
+      return { error: "Image is required" };
     } else if (!description) {
-      return "Item description is required";
+      return { error: "Item description is required" };
     } else {
-      return "Please enter all required fields";
+      return { error: "Please enter all required fields" };
     }
   }
   return null;
@@ -130,11 +133,11 @@ export function validateWorkFields(
 export function validateCartFields(customerId: ObjectId, productId: ObjectId) {
   if (!customerId || !productId) {
     if (!customerId) {
-      return "customerId is required";
+      return { error: "customerId is required" };
     } else if (!productId) {
-      return "productId is required";
+      return { error: "productId is required" };
     } else {
-      return "Please enter all required fields";
+      return { error: "Please enter all required fields" };
     }
   }
   return null;
