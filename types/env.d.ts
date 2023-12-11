4d5c82454becda99de95cf04584bc2737c928414
munene-m <munenenjue18@gmail.com>
2023-12-11 17:23:33 +0300

    chore: change the type of bcrypt_salt env

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,15 +1,16 @@
+interface EnvVariables {
+  MONGO_CONNECTION_URL: string | undefined;
+  JWT_SECRET: string | undefined;
+  BCRYPT_SALT: string | undefined;
+  CLOUDINARY_API_SECRET: string | undefined;
+  CLOUDNAME: string | undefined;
+  CLOUDINARY_API_KEY: string | undefined;
+  REDIS_URL: string | undefined;
+  SERVER_URL: string | undefined;
+}
 declare global {
   namespace NodeJS {
-    interface ProcessEnv {
-      MONGO_CONNECTION_URL: string | undefined;
-      JWT_SECRET: string | undefined;
-      BCRYPT_SALT: number | undefined;
-      CLOUDINARY_API_SECRET: string | undefined;
-      CLOUDNAME: string | undefined;
-      CLOUDINARY_API_KEY: string | undefined;
-      REDIS_URL: string | undefined;
-      SERVER_URL: string | undefined;
-    }
+    interface ProcessEnv extends EnvVariables {}
   }
 }
 
