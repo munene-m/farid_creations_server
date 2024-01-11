075b62475a810eae4277762266b0d12bdb2ce29b
munene-m <munenenjue18@gmail.com>
2024-01-11 13:33:26 +0300

    chore: only admin can get all users

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -7,9 +7,10 @@ import {
   getUsers,
 } from "../controllers/authController";
 import userProtect from "../middleware/authMiddleware";
+import adminProtect from "../middleware/adminMiddleware";
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/:customerId").get(userProtect, getUser);
-router.route("/").get(userProtect, getUsers);
+router.route("/").get(adminProtect, getUsers);
 
 export default router;
