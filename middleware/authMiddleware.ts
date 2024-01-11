301e25bcc6febf6dbaf310806eae00cf95ca7a75
munene-m <munenenjue18@gmail.com>
2024-01-11 13:04:54 +0300

    chore: restrict customers to access only their items and give admin unrestricted access

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,11 +1,10 @@
 import { Request, Response, NextFunction } from "express";
-
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt, { TokenExpiredError, JwtPayload } from "jsonwebtoken";
 import Auth from "../models/auth";
 import rateLimit from "express-rate-limit";
 
 interface DecodedToken extends JwtPayload {
-  userId: string; // Adjust the structure as needed based on your token
+  customerId: string; // Adjust the structure as needed based on your token
 }
 
 // Define a rate limit middleware for authenticated users
@@ -25,31 +24,60 @@ const authLimiter = rateLimit({
 
 const userProtect = async (req: Request, res: Response, next: NextFunction) => {
   let token: string;
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
 
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET || ""
-    ) as DecodedToken;
+    try {
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET || ""
+      ) as DecodedToken;
 
-    const user = await Auth.findById(decoded.id).select("-password");
-    if (user) {
-      req.user = { id: user._id, ...user };
-    }
+      const user = await Auth.findById(decoded.id).select("-password");
 
-    // Apply rate limiting for authenticated users
-    authLimiter(req, res, (err: any) => {
-      if (err) {
-        return res
-          .status(429)
-          .json({ error: "Rate limit exceeded. Try again later." });
+      if (!user) {
+        return res.status(401).json({ error: "Unauthorized attempt" });
       }
-      next();
-    });
+
+      req.user = { id: user._id, ...user };
+
+      // Check the role and grant or deny access
+      if (user.role === "admin") {
+        // Admin has unrestricted access
+        next();
+      } else {
+        // Regular users have restricted access
+        // Modify this based on your route requirements
+        // For example, you can check if the user has permission to access a specific route
+        if (
+          req.params.customerId &&
+          req.params.customerId !== user.id.toString()
+        ) {
+          return res.status(403).json({ error: "Forbidden" });
+        }
+        // Apply rate limiting for authenticated users
+        authLimiter(req, res, (err: any) => {
+          if (err) {
+            return res
+              .status(429)
+              .json({ error: "Rate limit exceeded. Try again later." });
+          }
+          next();
+        });
+      }
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        return res
+          .status(401)
+          .json({ error: "Token has expired. Please log in again" });
+      } else {
+        return res.status(401).json({ error: "Invalid token" });
+      }
+    }
   } else {
     return res.status(401).json({ error: "Unauthorized attempt" });
   }
