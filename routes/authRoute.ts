2f72fe9d069407c135b5ce1e3e8d0e26d2ea8c50
munene-m <munenenjue18@gmail.com>
2024-01-11 13:03:49 +0300

    chore: rename id to customerId and remove unused routes

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,19 +1,15 @@
 import express from "express";
 const router = express.Router();
 import {
-  createUser,
+  registerUser,
   loginUser,
-  registerAdmin,
-  loginAdmin,
   getUser,
   getUsers,
 } from "../controllers/authController";
 import userProtect from "../middleware/authMiddleware";
-router.route("/register").post(createUser);
+router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/register-admin").post(registerAdmin);
-router.route("/login-admin").post(loginAdmin);
-router.route("/:id").get(userProtect, getUser);
+router.route("/:customerId").get(userProtect, getUser);
 router.route("/").get(userProtect, getUsers);
 
 export default router;
