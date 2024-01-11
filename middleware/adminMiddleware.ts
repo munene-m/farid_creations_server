1731d5509ec4a21a052386ff6a6e14026ba6676a
munene-m <munenenjue18@gmail.com>
2024-01-11 12:59:29 +0300

    chore: only give access to users with admin role

--- middleware/adminMiddleware.ts
+++ middleware/adminMiddleware.ts
@@ -4,38 +4,41 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 interface DecodedToken extends JwtPayload {
   userId: string; // Adjust the structure as needed based on your token
 }
-export const adminProtect = async (
+const adminProtect = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  let token;
+  let token: string;
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
 
-    try {
-      const decoded = jwt.verify(
-        token,
-        process.env.JWT_SECRET as string
-      ) as DecodedToken;
-      const user = await Auth.findById(decoded.id).select("-password");
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || ""
+    ) as DecodedToken;
 
-      if (user && user.isAdmin) {
-        req.admin = user;
-        next();
-      } else {
-        return res.status(401).json({ error: "Unauthorized admin access" });
-      }
-    } catch (error) {
-      return res.status(401).json({ error: "Invalid token" });
-      // throw new Error("Invalid token");
+    const user = await Auth.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ error: "Unauthorized attempt" });
+    }
+
+    req.user = { id: user._id, ...user };
+
+    // Check if the user is an admin
+    if (user.role === "admin") {
+      next(); // Admin has unrestricted access
+    } else {
+      return res.status(403).json({ error: "Forbidden access" });
     }
   } else {
-    return res.status(401).json({ error: "No authorization without token" });
-    //   throw new Error();
+    return res.status(401).json({ error: "Unauthorized attempt" });
   }
 };
+
 export default adminProtect;
