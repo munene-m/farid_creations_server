be35bf16489f9d220aa463176b5e4b5cd184379f
munene-m <munenenjue18@gmail.com>
2024-05-20 18:22:36 +0300

    handle chpter stk push on the server

--- config/db.ts
+++ config/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 export async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_CONNECTION_URL || "");
-    console.log(`MongoDB connected successfully ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_CONNECTION_URL || "");
+    console.log("MongoDB connected successfully");
   } catch (err: any) {
     console.log(err.message);
     process.exit(1);
--- controllers/paymentController.ts
+++ controllers/paymentController.ts
@@ -1,6 +1,62 @@
 import { Request, Response } from "express";
 import Payment from "../models/payments";
 import logger from "../helpers/logging";
+import axios from "axios";
+import { customAlphabet } from "nanoid/non-secure";
+
+const apiKey = process.env.CHPTER_PUBLIC_KEY;
+export const initiatePayment = async (req: Request, res: Response) => {
+  const {
+    customerName,
+    location,
+    phoneNumber,
+    email,
+    total,
+    products,
+    callback,
+  } = req.body;
+
+  if (!customerName || !location || !phoneNumber || !email || !total) {
+    return res
+      .status(400)
+      .json({ message: "Please enter all required fields" });
+  }
+  try {
+    const alphabet =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const generateNanoid = customAlphabet(alphabet, 10); // Generates IDs of length 10
+    const transactionRef = generateNanoid();
+    const response = await axios.post(
+      "https://api.chpter.co/v1/initiate/mpesa-payment",
+      {
+        customer_details: {
+          full_name: customerName,
+          location,
+          phone_number: phoneNumber,
+          email,
+        },
+        products,
+        amount: {
+          currency: "KES",
+          total,
+        },
+        callback_details: {
+          transaction_reference: transactionRef,
+          callback_url: callback,
+        },
+      },
+      {
+        headers: {
+          "Api-Key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "An error occured.", error });
+  }
+};
 
 export async function handleCallback(req: Request, res: Response) {
   try {
